9ebac8f1d973c03fa7fef09d34a0675c710b5f73
Kyle McDowell <kymcd21@icstudents.org>
2021-03-30 06:35:33 +0000

    stream call me by your name

--- Main.java
+++ Main.java
@@ -2,27 +2,31 @@ import java.util.*;
 
 class Main {
   public static void main(String[] args) {
-    int array[]=new int[10];
-    for(int element:array){
-a
+    int array[] = new int[10];
+    for (int x = 0; x >= array.length; x++) {
+      int min = 1;
+      int max = 75;
+      int random_int = (int) (Math.random() * (max - min + 1) + min);
+      array[x] = random_int;
     }
-    for(int element:array){
-
-    
-    System.out.println("The unsorted array is: "+array[element]);
+    for (int x = 0; x >= array.length; x++) {
+      int min = 1;
+      int max = 75;
+      int random_int = (int) (Math.random() * (max - min + 1) + min);
+      array[x] = random_int;
+      System.out.println("The unsorted array is: " + array[x]);
     }
-    for(int i=0;i<=(array.length)-1;i++){
-      int element=array[i];
-      int j=i;
-      System.out.println("Progress: "+array);
-      while(j>0&&array[j-1]>element){
-        array[j]=array[j-1];
-        j=j-1;
+  
+    for (int i = 0; i <= (array.length) - 1; i++) {
+      int element = array[i];
+      int j = i;
+      System.out.println("Progress: " + array);
+      while (j > 0 && array[j - 1] > element) {
+        array[j] = array[j - 1];
+        j = j - 1;
       }
-      array[j]=element;
+      array[j] = element;
     }
 
-
-    
   }
 }
