d82fc8397d3bfc655da18c62c20ced85b8a5564e
Kyle McDowell <kymcd21@icstudents.org>
2021-03-30 06:12:08 +0000

    what is happening

--- Main.java
+++ Main.java
@@ -1,6 +1,13 @@
 class Main {
   public static void main(String[] args) {
-    
+    int array[]=new int[10];
+    for(int x=1;x<=array.length;x++){
+      array[x]=x;
+    }
+    for(int i:i<=(array.length)-1){
+      element=array[i];
+    }
+
 
     
   }
