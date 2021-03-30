71e105789013096016a8287bf19d0d04def7f4d4
Kyle McDowell <kymcd21@icstudents.org>
2021-03-30 06:43:34 +0000

    wait why did it work that quickly what did i do

--- Main.java
+++ Main.java
@@ -2,25 +2,17 @@ import java.util.*;
 
 class Main {
   public static void main(String[] args) {
+    Random rNGesus = new Random();
     int array[] = new int[10];
-    for (int x = 0; x >= array.length; x++) {
-      int min = 1;
-      int max = 75;
-      int random_int = (int) (Math.random() * (max - min + 1) + min);
-      array[x] = random_int;
+    for (int x = 0; x <= (array.length) - 1; x++) {
+      array[x] = rNGesus.nextInt(70);
+      System.out.println(array[x]);
     }
-    for (int x = 0; x >= array.length; x++) {
-      int min = 1;
-      int max = 75;
-      int random_int = (int) (Math.random() * (max - min + 1) + min);
-      array[x] = random_int;
-      System.out.println("The unsorted array is: " + array[x]);
-    }
-  
+    System.out.println("The unsorted array is: " + Arrays.toString(array));
     for (int i = 0; i <= (array.length) - 1; i++) {
       int element = array[i];
       int j = i;
-      System.out.println("Progress: " + array);
+      System.out.println("Progress: " + Arrays.toString(array));
       while (j > 0 && array[j - 1] > element) {
         array[j] = array[j - 1];
         j = j - 1;
